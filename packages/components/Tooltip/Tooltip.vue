fe33e9d10447832d75b0c7c9a269c52690a839f8
774604846@qq.com <774604846@qq.com>
2024-10-26 19:04:31 +0800

    Tooltip complete

---
+++ packages/components/Tooltip/Tooltip.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="ct-tooltip" ref="containerNode" v-on="outerEvents">
+    <div class="ct-tooltip__trigger" ref="_triggerNode" v-on="events" v-if="!virtualTriggering">
+      <slot></slot>
+    </div>
+    <slot name="default" v-else></slot>
+
+    <transition :name="transition" @aftct-leave="destroyPopperInstance">
+      <div class="ct-tooltip__popper" ref="popperNode" v-on="dropdownEvents" v-if="visible">
+        <slot name="content">
+          {{ content }}
+        </slot>
+        <div id="arrow" data-popper-arrow></div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import type { TooltipProps, TooltipEmits, TooltipInstance } from './types'
+import { debounce, isNil, type DebouncedFunc, bind } from 'lodash-es'
+import { computed, onUnmounted, ref, watch, watchEffect, type Ref } from 'vue'
+import { createPopper, type Instance } from '@popperjs/core'
+import { useClickOutside, useEventListener } from '@create-element/hooks'
+import useEvenstToTiggerNode from './useEventsToTriggerNode'
+
+interface _TooltipProps extends TooltipProps {
+  virtualRef?: HTMLElement | void
+  virtualTriggering?: boolean
+}
+
+defineOptions({
+  name: 'CtTooltip'
+})
+
+const props = withDefaults(defineProps<_TooltipProps>(), {
+  placement: 'bottom',
+  trigger: 'hover',
+  transition: 'fade',
+  showTimeOut: 0,
+  hideTimeOut: 0,
+})
+
+const emits = defineEmits<TooltipEmits>()
+const visible = ref(false)
+
+const events: Ref<Record<string, EventListener>> = ref({})
+const outerEvents: Ref<Record<string, EventListener>> = ref({})
+const dropdownEvents: Ref<Record<string, EventListener>> = ref({})
+
+const containerNode = ref<HTMLElement>()
+const _triggerNode = ref<HTMLElement>()
+const popperNode = ref<HTMLElement>()
+
+const triggerNode = computed(() => {
+  if (props.virtualTriggering) {
+    return (
+      (props.virtualRef as HTMLElement) ?? _triggerNode.value
+    )
+  }
+  return _triggerNode.value as HTMLElement
+})
+
+const popperOptions = computed(() => {
+  return {
+    placement: props.placement,
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 9]
+        }
+      }
+    ],
+    ...props.popperOptions
+  }
+})
+
+const openDelay = computed(() => props.trigger === 'hover' ? props.showTimeOut : 0)
+const closeDelay = computed(() => props.trigger === 'hover' ? props.hideTimeOut : 0)
+
+let openDebounce: DebouncedFunc<() => void> | void
+let closeDebounce: DebouncedFunc<() => void> | void
+
+function openFinal() {
+  closeDebounce?.cancel()
+  openDebounce?.()
+}
+
+function closeFinal() {
+  openDebounce?.cancel()
+  closeDebounce?.()
+}
+
+function togglePopper() {
+  visible.value ? closeFinal() : openFinal()
+}
+
+function setVisible(val: boolean) {
+  if (props.disabled) return
+  visible.value = val
+  emits('visible-change', val)
+}
+
+function attachEvents() {
+  if (props.disabled || props.manual) return
+  if (props.trigger === 'hover') {
+    events.value['mouseenter'] = openFinal
+    outerEvents.value['mouseleave'] = closeFinal
+    dropdownEvents.value['mouseenter'] = openFinal
+    return
+  }
+  if (props.trigger === 'click') {
+    events.value['click'] = togglePopper
+    return
+  }
+  if (props.trigger === 'contextmenu') {
+    events.value['contextmenu'] = (e) => {
+      e.preventDefault()
+      openFinal()
+    }
+    return
+  }
+}
+
+let popperInstance: null | Instance
+
+function destroyPopperInstance() {
+  if (isNil(popperInstance)) return
+
+  popperInstance.destroy()
+  popperInstance = null
+}
+
+function resetEvents() {
+  events.value = {}
+  outerEvents.value = {}
+  dropdownEvents.value = {}
+
+  attachEvents()
+}
+
+const show: TooltipInstance['show'] = openFinal
+const hide: TooltipInstance['hide'] = function () {
+  openDebounce?.cancel()
+  setVisible(false)
+}
+
+watch(visible, (val) => {
+  if (!val) return
+  if (triggerNode.value && popperNode.value) {
+    popperInstance = createPopper(triggerNode.value, popperNode.value, popperOptions.value)
+  }
+}, { flush: "post" })
+
+watch(() => props.manual, (isManual) => {
+  if (isManual) {
+    resetEvents()
+    return
+  }
+  attachEvents()
+})
+
+watch(() => props.trigger, (val, oldVal) => {
+  if (val === oldVal) return
+  openDebounce?.cancel()
+  visible.value = false
+  emits('visible-change', false)
+  resetEvents()
+})
+
+watchEffect(() => {
+  if (!props.manual) {
+    attachEvents()
+  }
+  openDebounce = debounce(bind(setVisible, null, true), openDelay.value)
+  closeDebounce = debounce(bind(setVisible, null, false), closeDelay.value)
+})
+
+useClickOutside(containerNode, () => {
+  emits('click-outside')
+  if (props.trigger === "hover" || props.manual) return
+
+  visible.value && closeFinal()
+})
+
+useEvenstToTiggerNode(props, triggerNode, events, () => {
+  openDebounce?.cancel()
+  setVisible(false)
+})
+
+onUnmounted(() => {
+  destroyPopperInstance()
+})
+
+defineExpose<TooltipInstance>({
+  show,
+  hide
+})
+</script>
+
+<style scoped>
+@import "./style.css";
+</style>
---
+++ packages/components/Tooltip/index.ts
@@ -0,0 +1,6 @@
+import Tooltip from './Tooltip.vue'
+import { withInstall } from '@create-element/utils'
+
+export const CtTooltip = withInstall(Tooltip)
+
+export * from './types'
